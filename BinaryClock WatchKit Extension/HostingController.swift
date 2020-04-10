951f62fe9f40821b1553626d4a5f5d6541c287eb
Corey Brummel-Smith <cdbs3@gatech.edu>
2020-04-10 18:11:38 -0400

    Initial Commit

---
+++ BinaryClock WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  BinaryClock WatchKit Extension
+//
+//  Created by Corey Brummel-Smith on 4/10/20.
+//  Copyright © 2020 Corey Brummel-Smith. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ BinaryClock WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  BinaryClock WatchKit Extension
+//
+//  Created by Corey Brummel-Smith on 4/10/20.
+//  Copyright © 2020 Corey Brummel-Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
